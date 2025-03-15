feb404f7f8fd7abe354f5e839ca007015dc1e5f8
dung-phan <dungphan@pm.me>
2025-03-15 12:14:48 +0100

    Uses cn util

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link'
 import BookCover from '@/components/BookCover'
 import { Calendar } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { clsx } from 'clsx'
+import { cn } from '@/lib/utils'
 
 const BookCard = ({ id, cover, is_loaned, title, genre }) => {
   return (
-    <li className={clsx(is_loaned && 'w-full md:w-52')}>
+    <li className={cn(is_loaned && 'w-full md:w-52')}>
       <Link
         href={`/book/${id}`}
-        className={clsx(is_loaned && 'w-full flex flex-col items-center')}
+        className={cn(is_loaned && 'w-full flex flex-col items-center')}
       >
         <BookCover cover={cover} variant="regular" />
-        <div className={clsx(!is_loaned && 'xs:max-w-40 max-w-28')}>
+        <div className={cn(!is_loaned && 'xs:max-w-40 max-w-28')}>
           <p className="mt-2 line-clamp-1 text-base font-semibold text-white xs:text-xl">
             {title}
           </p>
