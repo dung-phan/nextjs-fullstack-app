aa1e134db53adf64debfb6c4f3ad65f0e83ed6a7
dung-phan <dungphan@pm.me>
2025-04-11 11:47:25 +0200

    Formats file with Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,25 @@ import { SessionProvider } from 'next-auth/react'
 import { auth } from '@/auth'
 
 const SoDoSans = localFont({
-  src: [
-    { path: '/fonts/SoDoSans-Regular.ttf', weight: '400', style: 'normal' },
-    { path: '/fonts/SoDoSans-SemiBold.ttf', weight: '600', style: 'normal' },
-    { path: '/fonts/SoDoSans-Bold.ttf', weight: '700', style: 'normal' }
-  ]
+	src: [
+		{ path: '/fonts/SoDoSans-Regular.ttf', weight: '400', style: 'normal' },
+		{ path: '/fonts/SoDoSans-SemiBold.ttf', weight: '600', style: 'normal' },
+		{ path: '/fonts/SoDoSans-Bold.ttf', weight: '700', style: 'normal' }
+	]
 })
 
 export const metadata: Metadata = {
-  title: 'UniBook',
-  description: 'UniBook is a book borrowing university library management.'
+	title: 'UniBook',
+	description: 'UniBook is a book borrowing university library management.'
 }
 
-export default async function RootLayout ({ children }: PropsWithChildren) {
-  const session = await auth()
-  return (
-    <html lang="en">
-      <SessionProvider session={session}>
-        <body className={`${SoDoSans.className} antialiased`}>{children}</body>
-      </SessionProvider>
-    </html>
-  )
+export default async function RootLayout({ children }: PropsWithChildren) {
+	const session = await auth()
+	return (
+		<html lang='en'>
+			<SessionProvider session={session}>
+				<body className={`${SoDoSans.className} antialiased`}>{children}</body>
+			</SessionProvider>
+		</html>
+	)
 }
