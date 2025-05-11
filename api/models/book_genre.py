0c3b0820c5cb8443912247e8e151f424920d3437
dung-phan <dungphan@pm.me>
2025-05-11 12:19:12 +0200

    Format file

--- api/models/book_genre.py
+++ api/models/book_genre.py
@@ -9,6 +9,10 @@ from api.database import db
 class BookGenre(db.Model):
     __tablename__ = "book_genre"
 
-    book_id: so.Mapped[uuid.UUID] = so.mapped_column(sa.ForeignKey("book.id"), primary_key=True)
+    book_id: so.Mapped[uuid.UUID] = so.mapped_column(
+        sa.ForeignKey("book.id"), primary_key=True
+    )
 
-    genre_id: so.Mapped[uuid.UUID] = so.mapped_column(sa.ForeignKey("genre.id"), primary_key=True)
+    genre_id: so.Mapped[uuid.UUID] = so.mapped_column(
+        sa.ForeignKey("genre.id"), primary_key=True
+    )
